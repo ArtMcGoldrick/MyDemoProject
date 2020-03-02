ec1b9eb28bda799d6716d98bcf341895adfcbe9c
Art Mc Goldrick <S00186896@mail.itsligo.ie>
2020-03-02 17:36:28 +0000

    Add project files.

---
+++ MyDemoProject.Test/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MyDemoProject;
+
+namespace MyDemoProject.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public static void Test1()
+        {
+            //arrange
+            Program p1 = new Program();
+            //act
+            int actualresult = p1.Add(5, 10);
+            //assert
+            Assert.That(actualresult, Is.EqualTo(15)); //15 is expected answer
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            //arrange
+            Program p2 = new Program();
+            //act
+            int actualresult = p2.Add(5, 10);
+            //assert
+            Assert.That(actualresult, Is.EqualTo(25)); //15 is expected answer
+        }
+    }
+}
---
+++ MyDemoProject/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDemoProject
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Add(5, 8));
+        }
+
+        public int Add(int a, int b)
+        {
+            int actualresult;
+
+            actualresult = a + b;
+
+            return actualresult;
+        }
+    }
+}
